6dd63dca6d6d00b567cf40d397511f6c4d1a8b53
20ramachandrana <44482134+20ramachandrana@users.noreply.github.com>
2022-02-05 21:22:49 -0500

    Update game.js

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ const context = document.querySelector("canvas").getContext("2d");
 // context.canvas.height = 450;
 // context.canvas.width = 900;
 
-var cactus_url = "https://raw.github.com/sasana-uwu/hackher/blob/main/visuals/cactus_normal.png";
+var cactus_url = "https://raw.githubusercontent.com/sasana-uwu/hackher/main/visuals/cactus_normal.png";
 var image = new Image();
 image.src = cactus_url;
 // image.crossOrigin = true;
