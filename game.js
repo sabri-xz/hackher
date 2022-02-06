66bac0ba68d0bc837792a3a53b432c9d1215bce0
sasana-uwu <sasalabrina@gmail.com>
2022-02-05 21:19:03 -0500

    Update game.js

--- game.js
+++ game.js
@@ -3,10 +3,10 @@ const context = document.querySelector("canvas").getContext("2d");
 // context.canvas.height = 450;
 // context.canvas.width = 900;
 
-var cactus_url = "https://github.com/sasana-uwu/hackher/blob/main/visuals/cactus_normal.png?raw=true";
+var cactus_url = "https://raw.github.com/sasana-uwu/hackher/blob/main/visuals/cactus_normal.png";
 var image = new Image();
 image.src = cactus_url;
-image.crossOrigin = true;
+// image.crossOrigin = true;
 
 image.onload = function() {
     context.drawImage(
