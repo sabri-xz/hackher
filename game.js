029b3796632622ebb5baf78ee50f90ebbbd65802
20ramachandrana <44482134+20ramachandrana@users.noreply.github.com>
2022-02-06 02:02:40 -0500

    revert

--- game.js
+++ game.js
@@ -4,11 +4,8 @@ context.canvas.height = 450;
 context.canvas.width = 900;
 
 var cactus_url = "visuals/cactus_normal.png";
-var cactus_url_f = "visuals/cactus_flower.png";
-var cactus_url_w = "visuals/cactus_wilt.pmg";
-var water_url = "visuals/water.png";
-var meter_url = "visuals/water meter.pmg";
 var image = new Image();
+// image.crossOrigin = true;
 
 image.onload = function() {
     context.drawImage(image, 150, 0, 450, 450);
