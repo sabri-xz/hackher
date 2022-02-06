96a29d4a07cfdd6f399aa3b9f78badf6b37df70f
sasana-uwu <sasalabrina@gmail.com>
2022-02-05 20:16:50 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,4 +1,14 @@
 const context = document.querySelector("canvas").getContext("2d");
 
-context.canvas.height = 400;
-context.canvas.width = 1220;
+context.canvas.height = 600;
+context.canvas.width = 1240;
+
+const square = {
+    height: 32,
+    jumping: true,
+    width: 32,
+    x: 0,
+    xVelocity: 0,
+    y: 0,
+    yVelocity: 0
+  };
